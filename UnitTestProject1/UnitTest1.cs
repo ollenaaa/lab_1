35be1d128eba094ebf690be9d88e1438ed7d594d
ollenaaa <elenasazonets@gmail.com>
2022-11-22 00:14:36 +0200

    lab_1

---
+++ UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,25 @@
+﻿using AnalaizerClassLibrary;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+
+        public TestContext TestContext { get; set; }
+
+        [TestMethod]
+
+        [DataSource("Microsoft.VisualStudio.TestTools.DataSource.XML", "TestData.xml", "Row", DataAccessMethod.Sequential)]
+        public void CheckFormat()
+        {
+            string expression_new = Convert.ToString(TestContext.DataRow["expression"]);
+            string expected = Convert.ToString(TestContext.DataRow["expected"]);
+            AnalaizerClass.expression = expression_new;
+            string actual = AnalaizerClass.Format();
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
